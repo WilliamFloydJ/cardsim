afe4c69768a47fab433deb72f33ece79e05aa5a0
WilliamFloydJ <floydfamily71@gmail.com>
2025-09-14 01:23:35 -0600

    form

--- server/index.js
+++ server/index.js
@@ -23,49 +23,44 @@ app.get("/api/cards", async (req, res) => {
 
 // CREATE a new post
 app.post("/api/cards", async (req, res) => {
-  try {
-    const {
-      card_name,
-      card_types,
-      card_img,
-      card_power,
-      card_toughness,
-      card_totalmana,
-      card_red,
-      card_blue,
-      card_green,
-      card_black,
-      card_white,
-    } = req.body;
-
-    console.log(card_types);
-
-    const card_type = "";
-    const card_url = "";
-
-    // Use placeholders ($1, $2) to prevent SQL injection
-    const { rows } = await db.query(
-      `INSERT INTO cards(card_name, card_type, card_url, card_power, card_toughness, card_totalmana, card_red, card_blue, card_green, card_black, card_white)
-       VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *`,
-      [
-        card_name,
-        card_type,
-        card_url,
-        card_power,
-        card_toughness,
-        card_totalmana,
-        card_red,
-        card_blue,
-        card_green,
-        card_black,
-        card_white,
-      ]
-    );
-    res.status(201).json(rows[0]);
-  } catch (error) {
-    console.error("Error creating post:", error);
-    res.status(500).json({ error: "Failed to create post." });
-  }
+  console.log(req.body);
+  // try {
+  //   const {
+  //     card_name,
+  //     card_types,
+  //     card_img,
+  //     card_power,
+  //     card_toughness,
+  //     card_totalmana,
+  //     card_red,
+  //     card_blue,
+  //     card_green,
+  //     card_black,
+  //     card_white,
+  //   } = req.body;
+  //   console.log(card_types);
+  //   const card_type = "";
+  //   const card_url = "";
+  //   // Use placeholders ($1, $2) to prevent SQL injection
+  //   const { rows } = await db.query(
+  //     `INSERT INTO cards(card_name, card_type, card_url, card_power, card_toughness, card_totalmana, card_red, card_blue, card_green, card_black, card_white)
+  //      VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *`,
+  //     [
+  //       card_name,
+  //       card_type,
+  //       card_url,
+  //       card_power,
+  //       card_toughness,
+  //       card_totalmana,
+  //       card_red,
+  //       card_blue,
+  //       card_green,
+  //     ]
+  //   );
+  // } catch (error) {
+  //   console.error("Error creating post:", error);
+  //   res.status(500).json({ error: "Failed to create post." });
+  // }
 });
 
 // Serve static React files in production
