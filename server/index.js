a8b166b008ed4c38b822a5cf7ade323af6b8159a
WilliamFloydJ <floydfamily71@gmail.com>
2025-09-20 15:08:43 -0600

    test

--- server/index.js
+++ server/index.js
@@ -44,22 +44,8 @@ app.put("/api/cards", upload.none(), async (req, res) => {
     // Use placeholders ($1, $2) to prevent SQL injection
     const { rows } = await db.query(
       `UPDATE cards
-SET card_name = '${$1}' , card_type = ${$2}, card_url = '${$3}', card_power = ${$4}, card_toughness = ${$5}, card_totalmana = ${$6}, card_red = ${$7}, card_blue = ${$8}, card_green = ${$9}, card_black = ${$10}, card_white = ${$11}
-WHERE card_id = ${$12} RETURNING *;`,
-      [
-        card_name,
-        card_type,
-        card_url,
-        card_power,
-        card_toughness,
-        card_totalmana,
-        card_red,
-        card_blue,
-        card_green,
-        card_black,
-        card_white,
-        card_id,
-      ]
+SET card_name = '${card_name}' , card_type = ${card_type}, card_url = '${card_url}', card_power = ${card_power}, card_toughness = ${card_toughness}, card_totalmana = ${card_totalmana}, card_red = ${card_red}, card_blue = ${card_blue}, card_green = ${card_green}, card_black = ${card_black}, card_white = ${card_white}
+WHERE card_id = ${card_id} RETURNING *;`
     );
   } catch (error) {
     console.error("Error creating post:", error);
